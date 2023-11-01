0dcb638ea4b725338725d30fb520cc70962e2b6d
babayl0729 <babayl0729@gmail.com>
2023-10-31 21:19:55 -0600

    while loop

---
+++ while.py
@@ -0,0 +1,17 @@
+msg = input("what's the secret password?")
+while msg != "bananas":
+    print("WRONG")
+    msg = input("What's the secret password?")
+print("CORRECT!")
+
+num = 1
+while num < 11:
+    print(num)
+    num += 2
+
+n = 0 
+while n < 10:
+    for n in range(1,10):
+        print(n*"\U0001f600")
+    n+=1
+
